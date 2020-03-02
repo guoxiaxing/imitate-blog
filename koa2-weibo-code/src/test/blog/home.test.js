07a99cd63d7c148e3a3c15a1eea1c5de939cfd5f
guoxx03 <guoxx03@fenbi.com>
2020-03-02 17:55:45 +0800

    unit test

--- koa2-weibo-code/src/test/blog/home.test.js
+++ koa2-weibo-code/src/test/blog/home.test.js
@@ -31,3 +31,16 @@ test('创建一条微博 应该成功', async () => {
   // 记录微博ID
   BLOG_ID = res.body.data.id;
 });
+
+// 加载数据 应该成功
+
+test('加载数据 应该成功', async () => {
+  const res = await server.get('/api/blog/loadMore/0').set('Cookie', Z_COOKIE);
+  expect(res.body.errno).toBe(0);
+  const data = res.body.data;
+  expect(data).toHaveProperty('blogList');
+  expect(data).toHaveProperty('count');
+  expect(data).toHaveProperty('pageIndex');
+  expect(data).toHaveProperty('pageSize');
+  expect(data).toHaveProperty('isEmpty');
+});
